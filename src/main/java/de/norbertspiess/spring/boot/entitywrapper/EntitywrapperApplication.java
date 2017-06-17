3292b66a263b64587061a7a879006299637590c9
Norbert Spieß <norbertspiess@users.noreply.github.com>
2017-06-17 18:02:08 +0200

    project setup

---
+++ src/main/java/de/norbertspiess/spring/boot/entitywrapper/EntitywrapperApplication.java
@@ -0,0 +1,12 @@
+package de.norbertspiess.spring.boot.entitywrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntitywrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntitywrapperApplication.class, args);
+	}
+}
---
+++ src/test/java/de/norbertspiess/spring/boot/entitywrapper/EntitywrapperApplicationTests.java
@@ -0,0 +1,16 @@
+package de.norbertspiess.spring.boot.entitywrapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EntitywrapperApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
